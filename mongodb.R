8f15a5bfc5e5fd677d02146ab29f3ed287b9d52c
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-06 23:31:39 +0530

    Loaded the Large Datset

--- mongodb.R
+++ mongodb.R
@@ -3,3 +3,10 @@ library(dplyr)
 library(mongolite)
 library(lubridate)
 library(gridExtra)
+require(data.table)
+
+
+
+#loading the data-H1b Visa Petitions Data Set
+h1b=data.table::fread("C:/Users/hp/Downloads/h1b_kaggle.csv")
+
