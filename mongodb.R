335bbcf05ac83577cdf78bc28d465a4bb871cc2e
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-06 23:45:42 +0530

    Project RMD file added

--- mongodb.R
+++ mongodb.R
@@ -9,4 +9,14 @@ require(data.table)
 
 #loading the data-H1b Visa Petitions Data Set
 h1b=data.table::fread("C:/Users/hp/Downloads/h1b_kaggle.csv")
+names(h1b)#names of the columns
+
+#Now let's remove spaces in the column names to avoid any problems when we query it from MongoDB.
+
+names(h1b) = gsub(" ","",names(h1b)) 
+names(h1b)
+
+
+#Creating a Mongodb collection which is a collection of JSON documents in key-value pairs
+
 
